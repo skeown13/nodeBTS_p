ed26eb3f81bcfdfd0e54a3c5f4cbadee60289adb
skeown13 <code@keown.net>
2020-07-24 16:37:59 -0700

    basic server

--- events.js
+++ events.js
@@ -1,4 +1,5 @@
 const EventEmitter = require("events")
+const http = require("http")
 
 class Sales extends EventEmitter {
   constructor() {
@@ -24,3 +25,22 @@ myEmitter.emit("newSale", 9)
 
 ////////////////////
 
+const server = http.createServer()
+
+server.on("request", (req, res) => {
+  console.log("Request received!")
+  console.log(req.url)
+  res.end("Request received")
+})
+
+server.on("request", (req, res) => {
+  console.log("Another request")
+})
+
+server.on("close", () => {
+  console.log("Server closed")
+})
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for requests...")
+})
