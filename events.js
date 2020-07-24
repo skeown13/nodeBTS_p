6414cfc405ec8d363c31c7ab97941580cd845bae
skeown13 <code@keown.net>
2020-07-24 16:11:58 -0700

    can add properties to the emiter to be used in the callback function. also created a class that extended eventEmitter with ES6

--- events.js
+++ events.js
@@ -1,6 +1,12 @@
 const EventEmitter = require("events")
 
-const myEmitter = new EventEmitter()
+class Sales extends EventEmitter {
+  constructor() {
+    super() // get access to all the methods of the parent class
+  }
+}
+
+const myEmitter = new Sales()
 
 myEmitter.on("newSale", () => {
   console.log("There was a new sale!")
@@ -10,4 +16,11 @@ myEmitter.on("newSale", () => {
   console.log("Customer name: Stacy")
 })
 
-myEmitter.emit("newSale")
+myEmitter.on("newSale", stock => {
+  console.log(`There are now ${stock} items left in stock.`)
+})
+
+myEmitter.emit("newSale", 9)
+
+////////////////////
+
