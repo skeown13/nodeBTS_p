8d89e3954646aec692268926745b5d36b8060996
skeown13 <code@keown.net>
2020-07-24 15:49:29 -0700

    calling the crypto synchronously pulls it out of the eventloop. so any code in the eventloop has to wait for it to finish even though it finishes first

--- event-loop.js
+++ event-loop.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 const crypto = require("crypto")
 
 const start = Date.now()
-process.env.UV_THREADPOOL_SIZE = 3
+process.env.UV_THREADPOOL_SIZE = 4
 
 setTimeout(() => console.log("Timer 1 finished"), 0)
 setImmediate(() => console.log("Immediate 1 finished"))
@@ -16,18 +16,18 @@ fs.readFile("test-file.txt", () => {
 
   process.nextTick(() => console.log("Process.nextTick"))
 
-  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
-    console.log(Date.now() - start, "Password encrypted")
-  })
-  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
-    console.log(Date.now() - start, "Password encrypted")
-  })
-  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
-    console.log(Date.now() - start, "Password encrypted")
-  })
-  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
-    console.log(Date.now() - start, "Password encrypted")
-  })
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512")
+  console.log(Date.now() - start, "Password encrypted")
+  
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512")
+  console.log(Date.now() - start, "Password encrypted")
+  
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512")
+  console.log(Date.now() - start, "Password encrypted")
+  
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512")
+  console.log(Date.now() - start, "Password encrypted")
+
 })
 
 console.log("Hello from the top-level code")
