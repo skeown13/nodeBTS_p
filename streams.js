e0d19c137f51d82c4ab369fb491a302eb99934d3
skeown13 <code@keown.net>
2020-07-28 14:03:19 -0700

    changed from reading the large file and sending it all back at once as data, to making it into a stream and sending the information back in chunks

--- streams.js
+++ streams.js
@@ -3,11 +3,20 @@ const server = require("http").createServer()
 
 server.on("request", (req, res) => {
   // Solution 1
-  fs.readFile('test-file.txt', (err, data) => {
-    if (err) console.log (err)
-    res.end(data)
-  })
+  // fs.readFile('test-file.txt', (err, data) => {
+  //   if (err) console.log (err)
+  //   res.end(data)
+  // })
 
+  // Solution 2: Streams
+  const readable = fs.createReadStream("test-file.txt")
+  readable.on("data", chunk => {
+    res.write(chunk)
+  })
+  readable.on("end", () => {
+    // need this piece to send the information to the client
+    res.end()
+  })
 })
 
 server.listen(8000, "127.0.0.1", () => {
