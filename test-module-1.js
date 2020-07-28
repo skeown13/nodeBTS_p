3c47f79c99bfa29232f9bc3a59370063d0906608
skeown13 <code@keown.net>
2020-07-28 14:37:35 -0700

    assigning what we want directly to module.exports

--- test-module-1.js
+++ test-module-1.js
@@ -1,4 +1,22 @@
-class Calculator {
+// class Calculator {
+//   add(a, b) {
+//     return a + b
+//   }
+
+//   subtract(a, b) {
+//     return a - b
+//   }
+  
+//   multiply(a, b) {
+//     return a * b
+//   }
+
+//   divide(a, b) {
+//     return a / b
+//   }
+// }
+
+module.exports = class {
   add(a, b) {
     return a + b
   }
@@ -15,5 +33,3 @@ class Calculator {
     return a / b
   }
 }
-
-module.exports = Calculator
