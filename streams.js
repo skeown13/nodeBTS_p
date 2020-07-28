49635ce997833d0a53412816d517020d8a01cbaf
skeown13 <code@keown.net>
2020-07-28 14:13:37 -0700

    using the pipe() function - most desirable

--- streams.js
+++ streams.js
@@ -9,20 +9,25 @@ server.on("request", (req, res) => {
   // })
 
   // Solution 2: Streams
+  // const readable = fs.createReadStream("test-file.txt")
+  // readable.on("data", chunk => {
+  //   res.write(chunk)
+  // })
+  // readable.on("end", () => {
+  //   // need this piece to send the information to the client
+  //   res.end()
+  // })
+  // readable.on("error", err => {
+  //   console.log(err)
+  //   // Server error code
+  //   res.statusCode = 500
+  //   res.end("File not found!")
+  // })
+
+  // Solution 3: Pipe Operator (Solves 'backpressure' issue)
   const readable = fs.createReadStream("test-file.txt")
-  readable.on("data", chunk => {
-    res.write(chunk)
-  })
-  readable.on("end", () => {
-    // need this piece to send the information to the client
-    res.end()
-  })
-  readable.on("error", err => {
-    console.log(err)
-    // Server error code
-    res.statusCode = 500
-    res.end("File not found!")
-  })
+  readable.pipe(res)
+  // readableSource.pipe(writableDest)
 })
 
 server.listen(8000, "127.0.0.1", () => {
